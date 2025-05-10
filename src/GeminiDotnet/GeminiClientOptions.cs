c7f668af2036abbc16811efab744636ed5d925c1
Ryan Buckley <ryan@brownthwaite.com>
2025-05-10 11:29:09 +0100

    Add support for specifying a request timeout

--- src/GeminiDotnet/GeminiClientOptions.cs
+++ src/GeminiDotnet/GeminiClientOptions.cs
@@ -10,4 +10,12 @@ public sealed record GeminiClientOptions
     /// The model id to use unless overridden in by the caller.
     /// </summary>
     public string? ModelId { get; set; }
+
+    /// <summary>
+    /// The request timeout to use.
+    /// </summary>
+    /// <remarks>
+    /// This has no effect if you pass a <see cref="HttpClient"/> in yourself.
+    /// </remarks>
+    public TimeSpan? RequestTimeout { get; set; }
 }
