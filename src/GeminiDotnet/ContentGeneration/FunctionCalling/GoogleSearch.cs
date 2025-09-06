e69c8cca1fdf9682eea851ff9e3bdf659f031c97
Ryan Buckley <9486206+rabuckley@users.noreply.github.com>
2025-09-06 17:05:19 +0100

    Add TimeRangeFilter to GoogleSearch tool type (#37)

--- src/GeminiDotnet/ContentGeneration/FunctionCalling/GoogleSearch.cs
+++ src/GeminiDotnet/ContentGeneration/FunctionCalling/GoogleSearch.cs
@@ -1,3 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace GeminiDotnet.ContentGeneration.FunctionCalling;
 
-public sealed record GoogleSearch;
+/// <summary>
+/// GoogleSearch tool type. Tool to support Google Search in Model. Powered by Google.
+/// </summary>
+public sealed record GoogleSearch
+{
+    /// <summary>
+    /// Optional. Filter search results to a specific time range. If customers set a start time, they must set an end
+    /// time (and vice versa).
+    /// </summary>
+    [JsonPropertyName("timeRangeFilter")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public Interval? TimeRangeFilter { get; init; }
+}
