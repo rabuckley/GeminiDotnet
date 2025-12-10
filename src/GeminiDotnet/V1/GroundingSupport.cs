3dd19599c34e14122ea1f353e3b0443fe5bc2548
Ryan Buckley <ryan@brownthwaite.com>
2025-12-10 18:51:15 +0000

    Update to API revision 20251208

--- src/GeminiDotnet/V1/GroundingSupport.cs
+++ src/GeminiDotnet/V1/GroundingSupport.cs
@@ -8,7 +8,7 @@ namespace GeminiDotnet.V1;
 public sealed record GroundingSupport
 {
     /// <summary>
-    /// Confidence score of the support references. Ranges from 0 to 1. 1 is the
+    /// Optional. Confidence score of the support references. Ranges from 0 to 1. 1 is the
     /// most confident. This list must have the same size as the
     /// grounding_chunk_indices.
     /// </summary>
@@ -17,7 +17,7 @@ public sealed record GroundingSupport
     public ReadOnlyMemory<float> ConfidenceScores { get; init; }
 
     /// <summary>
-    /// A list of indices (into 'grounding_chunk') specifying the
+    /// Optional. A list of indices (into 'grounding_chunk') specifying the
     /// citations associated with the claim. For instance [1,3,4] means
     /// that grounding_chunk[1], grounding_chunk[3],
     /// grounding_chunk[4] are the retrieved content attributed to the claim.
